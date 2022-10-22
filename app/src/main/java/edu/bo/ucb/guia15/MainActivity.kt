5cba595dc45c4fdb788cce0bde0299381f4c4b66
Sergio <sergio.salinas.b@ucb.edu.bo>
2022-10-22 14:34:21 -0400

    Initial commit

---
+++ app/src/main/java/edu/bo/ucb/guia15/MainActivity.kt
@@ -0,0 +1,51 @@
+package edu.bo.ucb.guia15
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val button: Button get() = findViewById(R.id.button)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+
+            if(isConexion(applicationContext)) {
+                Toast.makeText(this, "Tiene acceso a internet", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No tiene acceso a internet", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+    fun isConexion(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw      = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                //for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            return connectivityManager.activeNetworkInfo?.isConnected ?: false
+        }
+    }
+
+}
+
+
+
